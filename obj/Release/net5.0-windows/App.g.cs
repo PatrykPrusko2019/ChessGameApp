7f0de7e4d18bcd76538354d603fca777fe731c38
Patryk <patrykprusko@gmai.com>
2022-07-12 01:02:22 +0200

    adds an exe file

--- obj/Release/net5.0-windows/App.g.cs
+++ obj/Release/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace ChessGameApp {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.16.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ChessGameApp {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.16.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
         public static void Main() {
             ChessGameApp.App app = new ChessGameApp.App();
             app.InitializeComponent();
